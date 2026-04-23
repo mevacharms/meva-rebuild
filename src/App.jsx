ded2b1b8c392e5876179299a95b65098486bcc03
Thanh Tran <thanh@example.com>
2026-04-22 23:19:43 -0400

    Update app to show Firestore name

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,5 @@
-import { useEffect } from "react";
-import { db } from "./firebase";
-import { doc, getDoc } from "firebase/firestore";
+import MevaPage from "./MevaPage";
 
 export default function App() {
-  useEffect(() => {
-    const testFetch = async () => {
-      try {
-        const ref = doc(db, "mevas", "test");
-        const snap = await getDoc(ref);
-
-        if (snap.exists()) {
-          console.log("DATA:", snap.data());
-        } else {
-          console.log("No document found");
-        }
-      } catch (err) {
-        console.error("ERROR:", err);
-      }
-    };
-
-    testFetch();
-  }, []);
-
-  return (
-    <div style={{ padding: 20 }}>
-      Firebase connected
-    </div>
-  );
+  return <MevaPage />;
 }
