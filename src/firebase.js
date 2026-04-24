ccc7ab3d7e5c84a55801df3b7ef10ce1b1df05c1
Thanh Tran <thanh@example.com>
2026-04-24 01:02:47 -0400

    Good spot to restart to, phone claim is working, browser claim is disabled

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "meva-clean.web.app",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
